d365635a69e86255682152103d965fcf342faa5a
Eva Clariana <eva.clariana@gmail.com>
2023-02-18 13:42:06 +0100

    change in main2

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Student student1 = new Student("Eva", 10);
-        Student student2 = new Student("Toni", 70);
+        Student student2 = new Student("Toni", 75);
         Student student3 = new Student("Laura", 20);
         Student student4 = new Student("Axel", 50);
 
